bb869453c6795a3805299a118cbaf4b6935c504a
程骞 <100251881+User-ChengQian@users.noreply.github.com>
2022-04-24 23:41:25 +0800

    Add files via upload

---
+++ day6/demo-4/src/components/Left.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="left-container">
+    <h3>Left 组件</h3>
+    <button @click="get">发起get请求</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async get() {
+      const { data: res } = await this.$http.get('/api/getbooks')
+      console.log(res)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.left-container {
+  padding: 0 20px 20px;
+  background-color: orange;
+  min-height: 250px;
+  flex: 1;
+}
+</style>
---
+++ day6/demo-4/src/components/Right.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="right-container">
+    <h3>Right 组件</h3>
+    <button @click="post">发起post请求</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async post() {
+      const { data: res } = await this.$http.post('/api/post', { name: 'zs', age: 20 })
+      console.log(res)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.right-container {
+  padding: 0 20px 20px;
+  background-color: lightskyblue;
+  min-height: 250px;
+  flex: 1;
+}
+</style>
